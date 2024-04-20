f450af4a9ff95212cb852d77a0ee54bcec64ff2b
chathudu <88680508+IT20253912@users.noreply.github.com>
2024-04-20 16:52:55 +0530

    sss

--- client/src/App.js
+++ client/src/App.js
@@ -1,198 +1,142 @@
-// import React from 'react';
-// import{BrowserRouter, Routes, Route} from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-// //pages and compoenets
-// // import Home from './pages/Home'
-// import Navbar from './components/Navbar'
-// import Sidebar from './components/Sidebar'
-// // import UserSelectPhotos from './pages/UserSelectPhotos'
-// import Footer from './components/Footer'
-// import HomeGallery from './components/homePageGallery'
-// import HomePageCard from './components/homePageCard'
-// import UserLoginPage from './components/UserManagement/userLogin'
+function PackageList() {
+  const [packages, setPackages] = useState([]);
+  const [updatedPackage, setUpdatedPackage] = useState({
+    packageID: '',
+    packageName: '',
+    packageType: '',
+    Description: '',
+    No_of_photographers: 0,
+    No_of_videographers: 0,
+    No_of_photos: 0,
+    Price: 0
+  });
 
-// //Import pages - Event Management
-// import EventPhotos from './pages/EventManagement/selectPhotosAlbum'
-// import Album from './pages/EventManagement/photoAlbum'
-// import CustomeEvent from './pages/EventManagement/userEventDetails'
-// import AllAlbums from './pages/EventManagement/allAlbums'
+  useEffect(() => {
+    // Fetch packages from backend when component mounts
+    axios.get('http://localhost:4001/api/eventPackage/package')
+      .then(response => {
+        setPackages(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching packages:', error);
+      });
+  }, []);
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <BrowserRouter>
-//         <Navbar/>
-//           <Routes>
-//             <Route
-//               path='/'
-//               element={<>
-//                 <HomeGallery />
-//                 <br></br>
-//                 <HomePageCard />
-//               </>}
-//             />
-//             <Route
-//               path='/signin'
-//               element={<UserLoginPage/>}
-//             />
-//             <Route
-//               path='/selectPhotos'
-//               element={<>
-//                 <Sidebar />
-//                 <EventPhotos/>
-//                 <br></br>
-//               </>}
-//             />
-//             <Route
-//               path='/viewVideos'
-//               element={<>
-//                 <Sidebar />
-//                 <br></br>
-//               </>}
-//             />
-//             <Route
-//               path='/viewAlbum'
-//               element={<>
-//                 <Sidebar />
-//                 <Album/>
-//                 <br></br>
-//               </>}
-//             />
-//             <Route
-//               path='/paymentHistory'
-//               element={<>
-//                 <Sidebar />
-//                 <br></br>
-//               </>}
-//             />
-//             <Route
-//               path='/cusEventInfo'
-//               element={<>
-//                 <CustomeEvent />
-//                 <br></br>
-//               </>}
-//             />
-//             <Route
-//               path='/manageAlbums'
-//               element={<>
-//                 <AllAlbums />
-//                 <br></br>
-//               </>}
-//             />
-//           </Routes>
-//           <br></br>
-//         <Footer/>
-//       </BrowserRouter>
-//     </div>
-//   );
-// }
+  const handleDelete = async (packageID) => {
+    try {
+      // Send delete request to backend
+      await axios.delete(`http://localhost:4001/api/eventPackage/package/${packageID}`);
+      
+      // Remove the deleted package from the state
+      setPackages(packages.filter(pkg => pkg.packageID !== packageID));
+    } catch (error) {
+      console.error('Error deleting package:', error);
+    }
+  };
 
-// export default App;
+  const handleUpdate = async () => {
+    try {
+      // Send update request to backend
+      await axios.put(`http://localhost:4001/api/eventPackage/package/${updatedPackage.packageID}`, updatedPackage);
+      
+      // Update the state to reflect the changes
+      setPackages(packages.map(pkg => pkg.packageID === updatedPackage.packageID ? updatedPackage : pkg));
+      
+      // Reset the form
+      setUpdatedPackage({
+        packageID: '',
+        packageName: '',
+        packageType: '',
+        Description: '',
+        No_of_photographers: 0,
+        No_of_videographers: 0,
+        No_of_photos: 0,
+        Price: 0
+      });
+    } catch (error) {
+      console.error('Error updating package:', error);
+    }
+  };
 
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import HomeGallery from './components/homePageGallery';
-import HomePageCard from './components/homePageCard';
-import SideBar from './components/Sidebar'
+  const handleChange = (e) => {
+    setUpdatedPackage({ ...updatedPackage, [e.target.name]: e.target.value });
+  };
 
-//User Management - Import Pages
-import UserLoginPage from './components/UserManagement/userLogin';
-
-//Event Management - Import Pages
-import EventPhotos from './pages/EventManagement/selectPhotosAlbum';
-import Album from './pages/EventManagement/photoAlbum';
-import CustomeEvent from './pages/EventManagement/userEventDetails';
-import AllAlbums from './pages/EventManagement/allAlbums';
-
-//Event Management - Import Components
-import CustomerDetails from './components/EventManagement/userEventDetails'; 
-import EditPhotoAlbum from './components/EventManagement/editPhotoAlbum'; 
-
-
-import AddPayment from './components/PaymentManagement/Addpayment'; 
-import AddPaymentn from './components/PaymentManagement/Addpaymentn'; 
-import ViewPayment from './components/PaymentManagement/Viewpayment'; 
-import DeletePayment from './components/PaymentManagement/Deletepayment'; 
-import Viewpackages from './components/PaymentManagement/Viewpackages'; 
-
-function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route
-            path='/'
-            element={<>
-              <HomeGallery />
-              <br />
-              <HomePageCard />
-            </>}
-          />
-          <Route
-            path='/signin'
-            element={<UserLoginPage />}
-          />
-          <Route
-            path='/selectPhotos'
-            element={<>
-              <SideBar/>
-              <EventPhotos />
-              <br />
-            </>}
-          />
-          <Route
-            path='/viewVideos'
-            element={<>
-              <SideBar/>
-              <br />
-            </>}
-          />
-          <Route
-            path='/viewAlbum'
-            element={<>
-              <SideBar/>
-              <Album />
-              <br />
-            </>}
-          />
-          <Route
-            path='/paymentHistory'
-            element={<>
-              <SideBar/>
-              <br />
-            </>}
-          />
-          <Route
-            path='/cusEventInfo'
-            element={<>
-              <CustomeEvent />
-              <br />
-            </>}
-          />
-          <Route
-            path='/manageAlbums'
-            element={<>
-              <AllAlbums />
-              <br />
-            </>}
-          />
-          <Route path='/customerDetails/:eventId' element={<CustomerDetails />} />
-          <Route path='/editAlbum/:photoAlbumId' element={<EditPhotoAlbum />} />
-          <Route path='/addpayment' element={<AddPayment />} />
-          <Route path='/addpaymentn' element={<AddPaymentn />} />
-          <Route path='/viewpayment' element={<ViewPayment />} />
-          <Route path='/payment' element={<DeletePayment />} />
-          <Route path='/viewpackages' element={<Viewpackages />} />
-        </Routes>
-        <br />
-        <Footer />
-      </BrowserRouter>
+    <div>
+      <h1>Package List</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Package ID</th>
+            <th>Package Name</th>
+            <th>Package Type</th>
+            <th>Description</th>
+            <th>No. of Photographers</th>
+            <th>No. of Videographers</th>
+            <th>No. of Photos</th>
+            <th>Price</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {packages.map(pkg => (
+            <tr key={pkg._id}>
+              <td>{pkg.packageID}</td>
+              <td>{pkg.packageName}</td>
+              <td>{pkg.packageType}</td>
+              <td>{pkg.Description}</td>
+              <td>{pkg.No_of_photographers}</td>
+              <td>{pkg.No_of_videographers}</td>
+              <td>{pkg.No_of_photos}</td>
+              <td>${pkg.Price}</td>
+              <td>
+                <button onClick={() => handleDelete(pkg.packageID)}>Delete</button>
+                <button onClick={() => setUpdatedPackage(pkg)}>Update</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <h2>Update Package</h2>
+      <div>
+        <label>Package ID:</label>
+        <input type="text" name="packageID" value={updatedPackage.packageID} onChange={handleChange} disabled />
+      </div>
+      <div>
+        <label>Package Name:</label>
+        <input type="text" name="packageName" value={updatedPackage.packageName} onChange={handleChange} />
+      </div>
+      <div>
+        <label>Package Type:</label>
+        <input type="text" name="packageType" value={updatedPackage.packageType} onChange={handleChange} />
+      </div>
+      <div>
+        <label>Description:</label>
+        <input type="text" name="Description" value={updatedPackage.Description} onChange={handleChange} />
+      </div>
+      <div>
+        <label>No. of Photographers:</label>
+        <input type="number" name="No_of_photographers" value={updatedPackage.No_of_photographers} onChange={handleChange} />
+      </div>
+      <div>
+        <label>No. of Videographers:</label>
+        <input type="number" name="No_of_videographers" value={updatedPackage.No_of_videographers} onChange={handleChange} />
+      </div>
+      <div>
+        <label>No. of Photos:</label>
+        <input type="number" name="No_of_photos" value={updatedPackage.No_of_photos} onChange={handleChange} />
+      </div>
+      <div>
+        <label>Price:</label>
+        <input type="number" name="Price" value={updatedPackage.Price} onChange={handleChange} />
+      </div>
+      <button onClick={handleUpdate}>Update Package</button>
     </div>
   );
 }
 
-export default App;
-
+export default PackageList;
---
+++ client/src/components/PaymentManagement/Editpackage.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Editpackage extends Component {
+  render() {
+    return (
+      <div>Editpackage</div>
+    )
+  }
+}
